fe107209cdcc47163d1cbff18725bc2aaa30a778
Magnus Green <magnus.h.green@gmail.com>
2012-03-23 01:05:42 +0100

    compacted AnagramsView

--- Anagrams-Web/Scripts/src/AnagramsView.js
+++ Anagrams-Web/Scripts/src/AnagramsView.js
@@ -4,10 +4,7 @@ var startup = function () { $(document).ready(function () {
 
     $.get("/data/wordlist.txt", function (data) {
         $("#loading").show();
-        var inputData = data.split("\n");
-        var anagramsMap = anagramsFinder.find(inputData);
-
-        _.each(anagramsMap, function (anagrams) {
+        _.each(anagramsFinder.find(data.split("\n")), function (anagrams) {
             if (anagrams.length > 1) {
                 $("#anagrams").append("<li class='anagram_group'>" + anagrams.join(" ") + "</li>");
                 total++;
